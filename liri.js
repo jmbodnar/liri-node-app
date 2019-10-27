8c4935de7cfd98e9a4a0fbb55bc2d58a04da13d4
jmbodnar <jmbodnar@protonmail.com>
2019-10-27 11:33:44 -0400

    Update error handling, add ops/bands.js

--- liri.js
+++ liri.js
@@ -1,68 +1,13 @@
-require('dotenv').config();
-const keys = require('./keys.js');
-const axios = require('axios');
+// require('dotenv').config();
+// const keys = require('./keys.js');
 
-const [command, value, three, four, five] = process.argv.slice(2);
-
-const getBandInfo = () => {
-  return axios
-    .get(
-      `https://rest.bandsintown.com/artists/${value}/events?app_id=codingbootcamp`
-    )
-    .then(response => {
-      console.log(`\n===== EVENTS LIST FOR ${value.toUpperCase()} =====\n`);
-      response.data.forEach(event => {
-        const date = new Date(event.datetime);
-        let datestring = `${String(date.getMonth()).padStart(
-          2,
-          '0'
-        )}/${date.getDate()}/${date.getFullYear()}`;
-        console.log(`\n\tVenue Name: ${event.venue.name}`);
-        console.log(
-          `\tEvent Location: ${
-            event.venue.city ? event.venue.city + ', ' : ''
-          }${event.venue.region ? event.venue.region + ', ' : ''}${
-            event.venue.country ? event.venue.country : ''
-          }`
-        );
-        console.log(`\tEvent Date: ${datestring}\n`);
-      });
-      console.log(`\n===== END EVENTS LIST FOR ${value.toUpperCase()} =====`);
-    });
-};
+const command = process.argv[2];
+const { getBandInfo } = require('./ops/bands');
 
 switch (command) {
   case 'concert-this':
     getBandInfo();
     break;
   default:
-    console.log("Fuck. It didn't work");
+    console.log('Crap! Nothing worked.');
 }
-
-// to access keys information...
-// var spotify = new Spotify(keys.spotify);
-
-// axios
-//   .get('https://en.wikipedia.org/wiki/Kudos_(granola_bar)')
-//   .then(function(response) {
-//     // If the axios was successful...
-//     // Then log the body from the site!
-//     console.log(response.data);
-//   })
-//   .catch(function(error) {
-//     if (error.response) {
-//       // The request was made and the server responded with a status code
-//       // that falls out of the range of 2xx
-//       console.log(error.response.data);
-//       console.log(error.response.status);
-//       console.log(error.response.headers);
-//     } else if (error.request) {
-//       // The request was made but no response was received
-//       // `error.request` is an object that comes back with details pertaining to the error that occurred.
-//       console.log(error.request);
-//     } else {
-//       // Something happened in setting up the request that triggered an Error
-//       console.log('Error', error.message);
-//     }
-//     console.log(error.config);
-//   });
---
+++ ops/bands.js
@@ -0,0 +1,46 @@
+const [command, ...value] = process.argv.slice(2);
+const band = value.join(' ');
+const axios = require('axios');
+
+const getBandInfo = () => {
+  return axios
+    .get(
+      `https://rest.bandsintown.com/artists/${encodeURIComponent(
+        band
+      )}/events?app_id=codingbootcamp`
+    )
+    .then(response => {
+      console.log(`\n===== EVENTS LIST FOR ${band.toUpperCase()} =====\n`);
+      response.data.forEach(event => {
+        const date = new Date(event.datetime);
+        let datestring = `${String(date.getMonth()).padStart(
+          2,
+          '0'
+        )}/${date.getDate()}/${date.getFullYear()}`;
+        console.log(`\n\tVenue Name: ${event.venue.name}`);
+        console.log(
+          `\tEvent Location: ${
+            event.venue.city ? event.venue.city + ', ' : ''
+          }${event.venue.region ? event.venue.region + ', ' : ''}${
+            event.venue.country ? event.venue.country : ''
+          }`
+        );
+        console.log(`\tEvent Date: ${datestring}\n`);
+      });
+      console.log(`\n===== END EVENTS LIST FOR ${band.toUpperCase()} =====`);
+    })
+    .catch(error => {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
+    });
+};
+
+module.exports = { getBandInfo };
